991e525bbce2ad7cfd4d501c6aba8f5ed7192cef
yasuyuki sato <godasper@gmail.com>
2021-12-15 18:00:46 +0900

    users一覧を取得

--- app/app.js
+++ app/app.js
@@ -1,9 +1,19 @@
+// expressのパッケージを読み込み
 const express = require("express");
 const app = express();
+// sqlite3のパッケージを読み込み
+const sqlite3 = require("sqlite3");
+const dbPath = "app/db/database.sqlite3";
 
 // ルーティングを指定
-app.get("/api/v1/hello", (req, res) => {
-  res.json({ message: "hello world" });
+app.get("/api/v1/users", (req, res) => {
+  const db = new sqlite3.Database(dbPath);
+
+  db.all("SELECT * FROM users", (err, rows) => {
+    res.json(rows);
+  });
+
+  db.close();
 });
 
 const port = process.env.Port || 3000;
